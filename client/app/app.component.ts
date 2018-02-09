b3a6b9a3be3a89ba4d2c1347802e5d6e07a30042
delosssh <delosssh@gmail.com>
2018-02-09 18:35:35 +0300

    updated

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor( ) { }
 
+  ngOnInit() {
+    
+  }
+  
 }
