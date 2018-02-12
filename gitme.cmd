1afdcf3a98970845318bb0af8587ce32723eed5f
delosssh <delosssh@gmail.com>
2018-02-12 21:16:22 +0300

    updated

--- gitme.cmd
+++ gitme.cmd
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "updated"
 git remote remove origin
-git remote add origin https://github.com/delosssh/getdropngo.git
+git remote add origin https://github.com/delosssh/15thparallel.git
 git push -u origin master
