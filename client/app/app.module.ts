07b3f7b234189d3eca5de335e02d23c49727962c
delosssh <delosssh@gmail.com>
2018-02-21 22:20:50 +0300

    updated

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     BrowserAnimationsModule,
     HttpModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false })
