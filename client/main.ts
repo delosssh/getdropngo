bf1f9ca75a74ff9271bc9c3c672e6a0374379266
delosssh <delosssh@gmail.com>
2018-02-22 07:50:48 +0300

    updated

--- client/main.ts
+++ client/main.ts
@@ -12,8 +12,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-.then(() => {
-  // if('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.register('worker-basic.min.js');
-  // }
-});
+.catch(err => console.log(err));
